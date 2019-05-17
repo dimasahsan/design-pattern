fa6c817b15b325166b582cae7a2d4dc7fafa2a4a
Dimas Maulana A <dimasmaulanaahsan@gmail.com>
2019-05-17 14:02:27 +0700

    Add get factory on Gui Factory

--- src/pattern/creational/abstractfactory/factories/GuiFactory.java
+++ src/pattern/creational/abstractfactory/factories/GuiFactory.java
@@ -1,9 +1,27 @@
 package pattern.creational.abstractfactory.factories;
 
+import pattern.creational.abstractfactory.products.OsType;
 import pattern.creational.abstractfactory.products.Button;
 import pattern.creational.abstractfactory.products.Checkbox;
 
-public interface GuiFactory {
-    Button createButton();
-    Checkbox createCheckbox();
+public abstract class GuiFactory {
+
+    static GuiFactory getFactory(String type) {
+        GuiFactory factory;
+        switch (type) {
+            case "Linux":
+                factory = new LinuxFactory();
+                break;
+            case "Windows":
+                factory = new WindowsFactory();
+                break;
+            default:
+                throw new IllegalArgumentException("No such Factory");
+        }
+        return factory;
+    }
+
+    abstract Button createButton();
+
+    abstract Checkbox createCheckbox();
 }
