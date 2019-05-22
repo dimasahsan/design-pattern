587d01710dfb69110795d0f833df213c05508590
Dimas Maulana A <dimasmaulanaahsan@gmail.com>
2019-05-22 11:11:56 +0700

    Add prototype design pattern

--- src/MainClass.java
+++ src/MainClass.java
@@ -4,6 +4,13 @@ import pattern.creational.factorymethod.creator.DealerPurbalingga;
 import pattern.creational.factorymethod.creator.DealerYogyakarta;
 import pattern.creational.factorymethod.product.Motorcycle;
 import pattern.creational.factorymethod.product.MotorcycleType;
+import pattern.creational.prototype.cache.BundledShapeCache;
+import pattern.creational.prototype.shape.Circle;
+import pattern.creational.prototype.shape.Rectangle;
+import pattern.creational.prototype.shape.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainClass {
     public static void main(String[] args) {
@@ -52,6 +59,79 @@ public class MainClass {
 
         System.out.println("==============================================================");
         System.out.println("================== End Of Abstract Factory ===================");
+        System.out.println("==============================================================");
+        System.out.println();
+        System.out.println();
+
+        System.out.println("==============================================================");
+        System.out.println("=============== Prototype (Creational Pattern)===============");
+        System.out.println("==============================================================");
+
+        List<Shape> shapes = new ArrayList<>();
+        List<Shape> shapesCopy = new ArrayList<>();
+
+        Circle circle = new Circle();
+        circle.x = 10;
+        circle.y = 20;
+        circle.radius = 15;
+        circle.color = "red";
+        shapes.add(circle);
+
+        Circle anotherCircle = (Circle) circle.clone();
+        shapes.add(anotherCircle);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.width = 10;
+        rectangle.height = 20;
+        circle.color = "blue";
+        shapes.add(rectangle);
+
+        for (Shape shape : shapes) {
+            shapesCopy.add(shape.clone());
+        }
+
+        for (int i = 0; i < shapes.size(); i++) {
+            if (shapes.get(i) != shapesCopy.get(i)) {
+                System.out.println(i + ": Shapes are different objects (yay!)");
+                if (shapes.get(i).equals(shapesCopy.get(i))) {
+                    System.out.println(i + ": And they are identical (yay!)");
+                } else {
+                    System.out.println(i + ": But they are not identical (booo!)");
+                }
+            } else {
+                System.out.println(i + ": Shape objects are the same (booo!)");
+            }
+        }
+
+        System.out.println();
+        System.out.println("==============================================================");
+        System.out.println();
+
+        BundledShapeCache cache = new BundledShapeCache();
+
+        Shape shape1 = cache.get("Big green circle");
+        Shape shape2 = cache.get("Medium blue rectangle");
+        Shape shape3 = cache.get("Medium blue rectangle");
+
+        if (shape1 != shape2 && !shape1.equals(shape2)) {
+            System.out.println("Big green circle != Medium blue rectangle (yay!)");
+        } else {
+            System.out.println("Big green circle == Medium blue rectangle (booo!)");
+        }
+
+        if (shape2 != shape3) {
+            System.out.println("Medium blue rectangles are two different objects (yay!)");
+            if (shape2.equals(shape3)) {
+                System.out.println("And they are identical (yay!)");
+            } else {
+                System.out.println("But they are not identical (booo!)");
+            }
+        } else {
+            System.out.println("Rectangle objects are the same (booo!)");
+        }
+
+        System.out.println("==============================================================");
+        System.out.println("===================== End Of Prototype ======================");
         System.out.println("==============================================================");
         System.out.println();
         System.out.println();
---
+++ src/pattern/creational/prototype/shape/Circle.java
@@ -0,0 +1,27 @@
+package pattern.creational.prototype.shape;
+
+public class Circle extends Shape {
+    public int radius;
+
+    public Circle() {
+    }
+
+    public Circle(Circle target) {
+        super(target);
+        if(target != null) {
+            this.radius = target.radius;
+        }
+    }
+
+    @Override
+    public Shape clone() {
+        return new Circle(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Circle) || !super.equals(o)) return false;
+        Circle circle = (Circle) o;
+        return circle.radius == radius;
+    }
+}
